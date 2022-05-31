6ba30b8976571c2b5c90a66401d8a8592555059c
Tivanov99 <trifon.ivanov21@gmail.com>
2022-05-31 17:03:58 +0300

    Added button to html

--- FuelComsuptionCalculator/app.js
+++ FuelComsuptionCalculator/app.js
@@ -4,3 +4,6 @@ let fuelLitters = document.querySelector("#litters")
 
 let resultItem = document.createElement("label");
 resultItem.textContent= `${(fuelLitters/distance) * 100}л/100км`;
+
+let boddy = document.querySelector("body");
+boddy.appendChild(resultItem);
