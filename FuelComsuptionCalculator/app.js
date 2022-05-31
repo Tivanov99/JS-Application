0d8bea8833ec9b764e7b49628cbd1c777fca5703
Tivanov99 <trifon.ivanov21@gmail.com>
2022-05-31 17:02:24 +0300

    Added new element which contains the result

--- FuelComsuptionCalculator/app.js
+++ FuelComsuptionCalculator/app.js
@@ -1,3 +1,6 @@
 let distance = document.querySelector("#distance");
 
 let fuelLitters = document.querySelector("#litters")
+
+let resultItem = document.createElement("label");
+resultItem.textContent= `${(fuelLitters/distance) * 100}л/100км`;
