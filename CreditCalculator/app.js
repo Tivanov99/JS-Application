7cfc970462f8ab72f18f6295f02b555240f7838d
Tivanov99 <trifon.ivanov21@gmail.com>
2022-05-31 23:34:54 +0300

    Update index.html

--- CreditCalculator/app.js
+++ CreditCalculator/app.js
@@ -8,12 +8,14 @@ let button = document.querySelector("#Interest");
 
 button.addEventListener('click',(e)=>{
     e.preventDefault();
+    let resultDiv= document.querySelector("#resultPlace");
+    
+    resultDiv.lastChild.remove();
 
     let resultElement = document.createElement('label');
-    resultElement.textContent = `${(months.value / 12) *  (price.value * (percentage.value/100))}`;
+    resultElement.textContent = `Лихвата в лева е : ${(months.value / 12) *  (price.value * (percentage.value/100))}лв`;
     resultElement.classList.add('result');
 
-    let resultDiv= document.querySelector("#resultPlace");
     resultDiv.appendChild(resultElement);
 })
 
