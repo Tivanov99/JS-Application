cc2f3cedac213b472155ced46f70b4ced7d09d97
Tivanov99 <trifon.ivanov21@gmail.com>
2022-05-31 23:15:24 +0300

    Update

--- CreditCalculator/app.js
+++ CreditCalculator/app.js
@@ -4,6 +4,10 @@ let percentage= document.querySelector("#YearPercentage");
 
 let months = document.querySelector("#Months");
 
+let button = document.querySelector("#Interest");
+
+
+
 let resultElement = document.createElement('label');
 
 resultElement.textContent = (months / 12) *  (price.value * (percentage.value/100));
