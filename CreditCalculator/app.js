9274503d78a95277abf5aa315ae0dcbee4bdccea
Tivanov99 <trifon.ivanov21@gmail.com>
2022-05-31 22:51:42 +0300

    Added new element for result

--- CreditCalculator/app.js
+++ CreditCalculator/app.js
@@ -1,3 +1,6 @@
-let price = document.querySelector("#price");
+let price = document.querySelector("#Price");
 
 let percentage= document.querySelector("#YearPercentage");
+
+let resultElement = document.createElement('label');
+resultElement.textContent = price.value * (percentage.value/100);
