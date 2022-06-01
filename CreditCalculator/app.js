cf3b73a281d02bed33c1b3651c1fe3c668ac6b9f
Tivanov99 <trifon.ivanov21@gmail.com>
2022-06-01 17:17:20 +0300

    Updates

--- CreditCalculator/app.js
+++ CreditCalculator/app.js
@@ -1,36 +1,36 @@
 let price = document.querySelector("#Price");
 
-let percentage= document.querySelector("#YearPercentage");
+let percentage = document.querySelector("#YearPercentage");
 
 let months = document.querySelector("#Months");
 
 let button = document.querySelector("#Interest");
 
-button.addEventListener('click',(e)=>{
-    e.preventDefault();
-    let resultDiv= document.querySelector("#resultPlace");
-    
-    if(resultDiv.childNodes.length > 1){
-        resultDiv.lastChild.remove();
-        resultDiv.lastChild.remove();
-    }
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  let resultDiv = document.querySelector("#resultPlace");
 
-    let yearInterest = document.createElement('label');
-    yearInterest.textContent = `Годишната лихвата в лева е : ${(price.value * (percentage.value/100))}лв`;
-    yearInterest.classList.add('result');
+  if (resultDiv.childNodes.length > 1) {
+    resultDiv.lastChild.remove();
+    resultDiv.lastChild.remove();
+    resultDiv.lastChild.remove();
+  }
 
-    let brElement = document.createElement('br');
+  let yearInterest = document.createElement("label");
+  yearInterest.textContent = `Годишната лихвата в лева е : ${
+    price.value * (percentage.value / 100)
+  }лв`;
+  yearInterest.classList.add("result");
 
-    let monthsInterest = document.createElement('label');
-    monthsInterest.textContent = `Месечната лихвата в лева е : ${(price.value * (percentage.value/100))/months.value}лв`;
-    monthsInterest.classList.add('result');
+  let brElement = document.createElement("br");
 
-    resultDiv.appendChild(yearInterest);
-    resultDiv.appendChild(brElement);
-    resultDiv.appendChild(monthsInterest);
-})
+  let monthsInterest = document.createElement("label");
+  monthsInterest.textContent = `Месечната лихвата в лева е : ${
+    (price.value * (percentage.value / 100)) / months.value
+  }лв`;
+  monthsInterest.classList.add("result");
 
-
-
-
-
+  resultDiv.appendChild(yearInterest);
+  resultDiv.appendChild(brElement);
+  resultDiv.appendChild(monthsInterest);
+});
